26ccd68fc0acfece86627c908a7675b304e90cf7
hllwwwrld <arkoah072@gmail.com>
2022-10-24 17:14:19 +0500

    created test for wfm shiftsApprove wfm job

---
+++ jobApproveTest.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+
+# ПОДКЛЮЧЕНИЕ К БД ВФМ
+def connIntgr():
+    return psycopg2.connect(database="outside_integration_t",
+                            user="a_kosolapov",
+                            password="TcmYez0uzXgK9",
+                            host="stage-t-db.goodt.me",
+                            port="5432")
+
+
+cursor = connWfm().cursor()
+cursor.execute("""
+select s.id from shift s
+where startdatetime  >= '2022-10-24'
+and startdatetime < '2022-10-25'
+and worked_status = 'NOT_APPROVED'
+""")
+shiftsToApprove = cursor.fetchall()
+shiftsToApprove = [str(i)[1:-2] for i in shiftsToApprove]
+shiftsToApprove = ', '.join(shiftsToApprove)
+cursor.execute("""
+select s.id from shift s
+where s.id in ({0})
+and (s.worked_status != 'APPROVED'
+or  updated::text not like '2022-10-24 23:59%')
+""".format(shiftsToApprove))
+res = cursor.fetchall()
+res = [str(i)[1:-2] for i in res]
+print(len(res), res)
