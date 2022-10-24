ca18c92ec8c97b48ebcda9adca169305aca3d83d
hllwwwrld <arkoah072@gmail.com>
2022-10-24 22:44:29 +0500

    refactor jobapprove

--- jobApproveTest.py
+++ jobApproveTest.py
@@ -10,22 +10,30 @@ def connWfm():
                             port="5432")
 
 
+def create_file():
+    cursor = connWfm().cursor()
+    cursor.execute("""
+    select s.id from shift s
+    where startdatetime  >= '2022-10-24'
+    and startdatetime < '2022-10-25'
+    and worked_status = 'NOT_APPROVED'
+    """)
+    shiftsToApprove = cursor.fetchall()
+    shiftsToApprove = [str(i)[1:-2] for i in shiftsToApprove]
+    shiftsToApprove = ', '.join(shiftsToApprove)
+    sa = open('sa.txt', 'w')
+    sa.write(shiftsToApprove)
+    print(shiftsToApprove)
+
+
+s = input()
 cursor = connWfm().cursor()
-cursor.execute("""
-select s.id from shift s
-where startdatetime  >= '2022-10-24'
-and startdatetime < '2022-10-25'
-and worked_status = 'NOT_APPROVED'
-""")
-shiftsToApprove = cursor.fetchall()
-shiftsToApprove = [str(i)[1:-2] for i in shiftsToApprove]
-shiftsToApprove = ', '.join(shiftsToApprove)
-cursor.execute("""
-select s.id from shift s
+cursor.execute(""" 
+select * from shift s
 where s.id in ({0})
 and (s.worked_status != 'APPROVED'
 or  updated::text not like '2022-10-24 23:59%')
-""".format(shiftsToApprove))
+""".format(s))
 res = cursor.fetchall()
 res = [str(i)[1:-2] for i in res]
 print(len(res), res)
